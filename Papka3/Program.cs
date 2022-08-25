c7382275618ec36eee5c333ebf1dfeb20506a153
Viktor Belous <vyntyk@yandex.ru>
2022-08-25 22:42:47 +0300

    Задача 52

---
+++ Papka3/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+Console.Clear();
+
+Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAverage()
+{
+   for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i, j];
+        }
+        Console.Write($"{sum / arr.GetLength(0)}; ");
+    }
+}
+Console.WriteLine("------------------");
+FillArray(arr);
+Console.WriteLine("------------------");
+FindAverage();
+
